b2205b5a48073623d2d74ee7929bc645e37a5967
Kevin Gawlik <kevin@gawlik.io>
2019-12-10 11:37:08 +0100

    eslint: init

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "svelte3"
+    ],
+    "overrides": [
+        {
+            "files": [ '**/*.svelte' ],
+            "processor": "svelte3/svelte3"
+        }
+    ],
+    "rules": {
+    }
+};
