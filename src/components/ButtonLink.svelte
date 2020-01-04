1d0d8fcf3b622074303177c74cbe4372e1262211
Kevin Gawlik <kevin@gawlik.io>
2020-01-04 23:12:32 +0100

    components: smarter ButtonLink

--- src/components/ButtonLink.svelte
+++ src/components/ButtonLink.svelte
@@ -1,15 +1,31 @@
 <script>
     import { link } from 'svelte-spa-router';
 
+    // Because Svelte doesn't allow to add css classes to a component, here's a workaround
+    let _class = '';
+    export { _class as class };
+
     export let name;
     export let url;
 </script>
 
-<a
-    href={url}
-    use:link
-    class="
-    btn
-">
-    { name }
-</a>
+{#if url.startsWith('http')}
+    <a
+        href={url}
+        class="
+        btn
+        {_class}
+    ">
+        { name }
+    </a>
+{:else}
+    <a
+        href={url}
+        use:link
+        class="
+        btn
+        {_class}
+    ">
+        { name }
+    </a>
+{/if}
