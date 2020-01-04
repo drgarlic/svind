4293dca135dd9368d37d7d6cb2bd2d4d6cd3d227
Kevin Gawlik <kevin@gawlik.io>
2020-01-04 23:19:00 +0100

    sw: polishing registration

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,22 @@
     import Router from 'src/router/Router.svelte';
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then((registration) => {
-                console.log('Registration successful, scope is:', registration.scope);
-            })
-            .catch((error) => {
-                console.log('Service worker registration failed, error:', error);
-            });
+        if (process.env.NODE_ENV === 'production') {
+            navigator.serviceWorker.register('/service-worker.js');
+        } else {
+            navigator.serviceWorker.getRegistrations()
+                .then((registrations) => {
+                    registrations.forEach((registration) => {
+                        registration.unregister();
+                    });
+                    return registrations.length;
+                })
+                .then((len) => {
+                    if (len > 0) {
+                        location.reload();
+                    }
+                });
+        }
     }
 </script>
 
